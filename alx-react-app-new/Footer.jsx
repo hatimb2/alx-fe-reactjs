06f102c8be4d67413697a0bc782e40725fe6bd2e
hatimbr <hatim-boutejdir@hotmail.com>
2024-11-16 19:35:50 +0100

    done

---
+++ alx-react-app-new/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(css) {
+    return(
+        <footer style={{ backgroundColor: 'grey', color: 'black', textAlign: 'center'}}>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    )
+    
+}
---
+++ alx-react-app-new/MainContent.jsx
@@ -0,0 +1,8 @@
+function MainContent(css) {
+    return(
+        <main style={{ backgroundColor: 'white', color: 'wblack', textAlign: 'center'}}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+    
+}
