6793e3c9dd56d618c7e1d510d35e6b622f3e8135
hatimb2 <boutejdirhatim2@gmail.com>
2024-11-17 23:55:41 +0100

    donne

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,3 +1,15 @@
-import React from "react";
-const UserContext = React.createContext();
-export default UserContext; 
+import React, { useContext } from 'react';
+import UserContext from './UserContext'; 
+
+function UserDetails() {
+  const userData = useContext(UserContext); 
+
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserDetails;
