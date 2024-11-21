3cbf16bd092f46b0538d1e157fb09019678edf9d
hatimbr <hatim-boutejdir@hotmail.com>
2024-11-21 20:25:47 +0100

    ok

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,16 +2,17 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ background: '#333', padding: '10px' }}>
-      <ul style={{ listStyle: 'none', display: 'flex', justifyContent: 'space-around' }}>
-        <li><Link to="/" style={{ color: '#fff', textDecoration: 'none' }}>Home</Link></li>
-        <li><Link to="/about" style={{ color: '#fff', textDecoration: 'none' }}>About</Link></li>
-        <li><Link to="/services" style={{ color: '#fff', textDecoration: 'none' }}>Services</Link></li>
-        <li><Link to="/contact" style={{ color: '#fff', textDecoration: 'none' }}>Contact</Link></li>
+    <nav style={{ padding: '20px', backgroundColor: '#333', color: '#fff' }}>
+      <ul style={{ listStyleType: 'none', display: 'flex', gap: '10px' }}>
+        <li><Link to="/" style={{ color: '#fff' }}>Home</Link></li>
+        <li><Link to="/about" style={{ color: '#fff' }}>About</Link></li>
+        <li><Link to="/services" style={{ color: '#fff' }}>Services</Link></li>
+        <li><Link to="/contact" style={{ color: '#fff' }}>Contact</Link></li>
       </ul>
     </nav>
   );
 }
 
 export default Navbar;
+
 
