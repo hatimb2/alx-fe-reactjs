8bd0470d817fbe661197f22b9c5872c391fffad5
hatimbr <hatim-boutejdir@hotmail.com>
2024-11-23 21:09:47 +0100

    ok

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -1,17 +1,22 @@
 import { useRecipeStore } from './recipeStore';
 
-  const RecipeDetails = ({ recipeId }) => {
-    const recipe = useRecipeStore(state =>
-      state.recipes.find(recipe => recipe.id === recipeId)
-    );
+const RecipeDetails = ({ recipeId }) => {
+  const recipe = useRecipeStore(state =>
+    state.recipes.find(recipe => recipe.id === recipeId)
+  );
 
-    return (
-      <div>
-        <h1>{recipe.title}</h1>
-        <p>{recipe.description}</p>
-        {/* Render EditRecipeForm and DeleteRecipeButton here */}
-      </div>
-    );
-  };
+  if (!recipe) {
+    return <p>Recipe not found</p>;
+  }
 
-export default EditRecipeForm;
+  return (
+    <div>
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      {/* Render EditRecipeForm and DeleteRecipeButton here */}
+    </div>
+  );
+};
+
+export default RecipeDetails;
+
