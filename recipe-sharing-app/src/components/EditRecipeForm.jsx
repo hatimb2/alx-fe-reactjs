1c00ee9c62b15c45fdbc7faeb71614add72ddc2d
hatimbr <hatim-boutejdir@hotmail.com>
2024-11-23 21:15:33 +0100

    ok

--- recipe-sharing-app/src/components/EditRecipeForm.jsx
+++ recipe-sharing-app/src/components/EditRecipeForm.jsx
@@ -12,9 +12,9 @@ const RecipeDetails = ({ recipeId }) => {
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
-      <Link to={`/edit/${recipe}`}>Edit Recipe</Link>
+      <Link to={`/edit/${recipe.id}`}>Edit Recipe</Link>
       {/* Render EditRecipeForm and DeleteRecipeButton here */}
-      <DeleteRecipeButton recipe={recipe} />
+      <EditRecipeForm recipeId={recipe.id} />
     </div>
   );
 };
