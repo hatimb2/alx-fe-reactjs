a42cddbc4034f8d4b695207730495e642b327a83
hatimbr <hatim-boutejdir@hotmail.com>
2024-11-25 19:47:19 +0100

    ok

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,19 +1,29 @@
 import create from 'zustand';
 
 const useRecipeStore = create(set => ({
-  recipes: [],
-  favorites: [],
+  recipes: [], // Existing recipes state
+  favorites: [], // Existing favorites state
+  recommendations: [], // Existing recommendations state
+
+  // Function to add a new recipe to the store
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+
+  // Function to set the recipes (this can be used for bulk updating)
+  setRecipes: (recipes) => set({ recipes }),
+
+  // Function to add a recipe to favorites
   addFavorite: (recipeId) => set(state => ({ favorites: [...state.favorites, recipeId] })),
-  removeFavorite: (recipeId) => set(state => ({
-    favorites: state.favorites.filter(id => id !== recipeId)
-  })),
-  recommendations: [],
+
+  // Function to remove a recipe from favorites
+  removeFavorite: (recipeId) => set(state => ({ favorites: state.favorites.filter(id => id !== recipeId) })),
+
+  // Function to generate recommendations based on favorites
   generateRecommendations: () => set(state => {
-    const recommended = state.recipes.filter(recipe =>
-      state.favorites.includes(recipe.id) && Math.random() > 0.5
-    );
+    const recommended = state.recipes.filter(recipe => state.favorites.includes(recipe.id) && Math.random() > 0.5);
     return { recommendations: recommended };
-  }),
+  })
 }));
 
 export default useRecipeStore;
+
+
