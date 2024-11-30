7ed0179be0de02d9ba2b686b75db3de19dc1d8c6
hatimbr <hatim-boutejdir@hotmail.com>
2024-11-30 19:18:22 +0100

    done

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import formikForm from './components/formikForm'
-import RegistrationForm from './components/RegistrationForm '
+import formikForm from './components/formikForm.jsx'
+import RegistrationForm from './components/RegistrationForm .jsx'
 import './App.css'
 
 function App() {
