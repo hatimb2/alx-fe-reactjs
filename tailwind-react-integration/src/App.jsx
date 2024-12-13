5d19de582f6d4277d6754d0bbb15ab82c79b793e
hatimbr <hatim-boutejdir@hotmail.com>
2024-12-13 20:53:36 +0100

    done

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,17 +1,27 @@
-function UserProfile() {
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import UserProfile from './components/UserProfile'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
   return (
-    <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
-      <img 
-        src="https://via.placeholder.com/150" 
-        alt="User" 
-        className="w-24 h-24 sm:w-36 sm:h-36 rounded-full mx-auto" 
-      />
-      <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-sm sm:text-base text-gray-600">
-        Developer at Example Co. Loves to write code and explore new technologies.
-      </p>
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      
+    </>
+  )
 }
 
-export default UserProfile;
+export default App
