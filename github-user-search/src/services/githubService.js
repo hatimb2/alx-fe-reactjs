7e34ddb6408baaa6e07325400b2ed77560ce28b8
hatimb2 <boutejdirhatim2@gmail.com>
2024-12-23 00:40:24 +0100

    done

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchUserData = async (username) => {
   try {
-    const response = await axios.get(`https://api.github.com/users/${username}`);
+    const response = await axios.get(`https://api.github.com/users/{hatimb2}`);
     return response.data;
   } catch (error) {
     throw new Error('User not found');
