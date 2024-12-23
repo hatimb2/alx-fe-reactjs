f5043019e8e267c47f80192d937a48f12a3a3d05
hatimb2 <boutejdirhatim2@gmail.com>
2024-12-23 01:09:03 +0100

    done

--- github-user-search/src/index.jsx
+++ github-user-search/src/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css'; // Make sure this line is present
+import ReactDOM from 'react-dom/client'; // Import from 'react-dom/client' instead
+import index from'github-user-search\src\index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <Router>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
