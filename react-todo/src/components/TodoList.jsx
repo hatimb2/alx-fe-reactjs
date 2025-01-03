07dc50a7035eda24b2d2ec6fda57689303bd0deb
hatimbr <hatim-boutejdir@hotmail.com>
2025-01-03 22:35:56 +0100

    done

--- react-todo/src/components/TodoList.jsx
+++ react-todo/src/components/TodoList.jsx
@@ -1,65 +1,54 @@
-import React,{useState} from "react";
+// src/components/TodoList.jsx
+import React, { useState } from 'react';
 
-function TodoList() {
+const TodoList = () => {
   const [todos, setTodos] = useState([
-    { id: 1, text: 'Learn React', completed: false },
-    { id: 2, text: 'Learn JavaScript', completed: false },
+    { text: 'Learn React', completed: false },
+    { text: 'Learn JavaScript', completed: false },
   ]);
   const [newTodo, setNewTodo] = useState('');
 
-  // Add a new todo
   const addTodo = () => {
-    if (newTodo.trim()) {
+    if (newTodo) {
       setTodos([
         ...todos,
-        { id: Date.now(), text: newTodo, completed: false },
+        { text: newTodo, completed: false },
       ]);
       setNewTodo('');
     }
   };
 
-  // Toggle completion of a todo
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
+  const toggleTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
   };
 
-  // Delete a todo
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id));
+  const deleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
   };
 
   return (
     <div>
       <h1>Todo List</h1>
-      
-      {/* Input to add new todo */}
       <input
         type="text"
+        placeholder="Add a new todo"
         value={newTodo}
         onChange={(e) => setNewTodo(e.target.value)}
-        placeholder="Add a new todo"
       />
       <button onClick={addTodo}>Add Todo</button>
-
-      {/* Display the list of todos */}
       <ul>
-        {todos.map((todo) => (
-          <li
-            key={todo.id}
-            className={todo.completed ? 'completed' : ''}
-            onClick={() => toggleTodo(todo.id)}
-          >
-            <span>{todo.text}</span>
-            <button onClick={(e) => { e.stopPropagation(); deleteTodo(todo.id); }}>Delete</button>
+        {todos.map((todo, index) => (
+          <li key={index} className={todo.completed ? 'completed' : ''}>
+            <span onClick={() => toggleTodo(index)}>{todo.text}</span>
+            <button onClick={() => deleteTodo(index)}>Delete</button>
           </li>
         ))}
       </ul>
     </div>
   );
-}
+};
 
 export default TodoList;
